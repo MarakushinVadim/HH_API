fce8d5345166f75c198ee233760559b18e9bcc05
MarakushinVadim <154693061+MarakushinVadim@users.noreply.github.com>
2024-02-26 22:55:48 +0300

    Delete tests/test_JSON_saver.py

--- tests/test_JSON_saver.py
+++
@@ -1,9 +0,0 @@
-from src.HeadHunterAPI import Vacancy
-from src.JSON_saver import JSONSaver
-from data import *
-
-json_saver = JSONSaver()
-vacancy = Vacancy('vak', 'www', {'from': 0, 'to': 0, 'currency': 'RUR', 'gross': False}, 'описание')
-
-def test_add_vacancy():
-    json_saver.add_vacancy(vacancy, 'data/all.txt')
